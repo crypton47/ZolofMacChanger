9427b2c86a8a46c83b7823254d4d6656da774a4e
crypton47 <39877177+crypton47@users.noreply.github.com>
2019-07-18 16:20:59 +0000

    Add files via upload

--- myMacChanger.py
+++ myMacChanger.py
@@ -9,30 +9,35 @@ def slowprinting(s, t):
         sys.stdout.flush()
         time.sleep(t)
 if __name__ == "__main__":
-    slowprinting("# S T  A  R  T  I  N  G ! ", 0.05)
+    os.system('clear')
+    print("\n \n \n \n \n ")
+    slowprinting("\033[91m [!]  S T A R T I N G ... \033[93m", 0.05)
     time.sleep(1)
-    # os.system('clear')
     slowprinting('''
-                                ||\\      //||
-                                || \\    // ||
-                                ||  \\  //  ||
-                                ||   \\//   ||
-                                ||         ||R.Z0lof_______________________________________ (Version 0.1)
+               ||\\      //||
+               || \\    // ||
+               ||  \\  //  ||
+               ||   \\//   ||
+               ||         ||acChanger. (Version 0.1 by Z0lof47)
     ''', 0.0001)
-    print("[------------------------[1] Show my @MAC ----------------------------------------------]")
-    print("[------------------------[2] Change your @MAC Randomly ---------------------------------]")
-    print("[------------------------[3] Change your @MAC Custumly ---------------------------------]")
-    print("[---- -------------------[99] EXIT -----------------------------------------------------]")
-    choice = str(input("Make your choice : "))
+    print("                \033[91m  [1] \033[97m  Show @MAC")
+    print("                \033[91m  [2] \033[97m  Change @MAC Randomly")
+    print("                \033[91m  [3] \033[97m  Change @MAC Custumly ")
+    print("                \033[91m  [99] \033[97m  EXIT")
+    print(" ")
+    choice = str(input(" \033[91m  [?] \033[97m >>> Make your choice : "))
     if choice == "1":
-        print("Your  device eth0 MAC @ is ")
-        os.system('macchanger -s eht0')
-        os.system('macchanger -s wlan0')
-        print(" ")
+        print("eth0's MAC @is ")
+        os.system('macchanger -s eth0')
+        # os.system('macchanger -s wlan0')
+        input("Press any key to continue")
+        os.system('python3 myMacChanger.py')
     elif choice == "2":
         print("Changing  Randomly your @MAC ")
-        os.system('ifconfig eth0 down | macchanger -r eth0 | ifconfig eth0 up | macchanger -s eht0 ')
+        os.system('ifconfig eth0 down|macchanger -r eth0')
         print(" ")
+        os.system('ifconfig eth0 up')
+        os.system('macchanger -s eth0')
     elif choice == "3":
         print("Changing Customly your @MAC ")
         newMac = str(input("Enter the new @MAC using the format : XX:XX:XX:XX:XX:XX \n"))
